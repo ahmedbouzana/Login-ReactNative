b88c3cb33af76e865de5957a1f67d526434ab5c0
ahmedbouzana <ah.bouzana@gmail.com>
2019-04-23 10:03:17 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import {createStackNavigator } from "react-navigation"
+import Home from "./app/components/Home"
+import Dashboard from "./app/components/Dashboard"
+
+export default createStackNavigator ({
+home:Home,
+dashboard:Dashboard
+})
---
+++ app/components/Home/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import {View, Text, TextInput} from 'react-native'
+
+class Home extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Login into the appliation</Text>
+                <TextInput/>
+                <TextInput/>
+            </View>
+        )
+    }
+}
+
+export default Home
